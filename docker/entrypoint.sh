aef78d4f33bd34a9bf81b2df0640b7366568a5b1
Alexander Kahl <a.kahl@communicatio.systems>
2019-05-24 18:30:38 +0200

    Feature/dockerize

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-unset BUNDLE_PATH
-unset BUNDLE_BIN
+DB=${DB_HOST:-db:5432}
+
+dockerize -wait tcp://$DB -timeout 30s
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
 npm set audit false
-rake db:create
 rake db:migrate
 
 exec "$@"
