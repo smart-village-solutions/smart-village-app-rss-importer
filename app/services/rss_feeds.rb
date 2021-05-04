1c2d08ddd86d8f167c9d500bdfad7d3a22c1ed22
Marco Metz <mm@tpwd.de>
2021-05-04 10:08:25 +0200

    Load Feed Data from Dashboard Server

--- app/services/rss_feeds.rb
+++ app/services/rss_feeds.rb
@@ -1,6 +1,17 @@
 class RssFeeds
   def self.import
-    list_of_feed_urls = Rails.application.credentials.rss_feeds
+    begin
+      server_url = Rails.application.credentials.cronjob_service[:server_url]
+      project_id = Rails.application.credentials.cronjob_service[:project_id]
+      url = "#{server_url}/api/v1/settings/#{project_id}"
+      response = ApiRequestService.new(url, nil, nil, nil, {}).get_request.try(:body)
+      response = JSON.parse(response)
+
+      list_of_feed_urls = response["rss_feeds"]
+    rescue
+      list_of_feed_urls = []
+    end
+
     list_of_feed_urls.each do |feed|
       Importer.new(feed: feed)
     end
